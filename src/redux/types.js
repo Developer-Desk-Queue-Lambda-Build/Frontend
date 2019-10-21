9abc13878224884667d3c9a14612766a955bbcbc
Victor Arowo <arowovici@gmail.com>
2019-10-21 09:39:55 +0100

    Initial Files from Redux

---
+++ src/redux/types.js
@@ -0,0 +1,11 @@
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
+
+export const CREATE_TICKET = 'CREATE_TICKET';
+export const GET_OPEN_TICKETS = 'GET_OPEN_TICKETS';
+export const VIEW_TICKET = 'VIEW_TICKET';
+
+export const GET_ALL_TICKETS = 'GET_ALL_TICKETS';
+export const CHANGE_TICKET_STATUS = 'CHANGE_TICKET_STATUS';
+export const OWN_TICKET = 'OWN_TICKET';
+export const DELETE_OWNERSHIP = 'DELETE_OWNERSHIP';
