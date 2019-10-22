395053fe2d70afe216f8fe503ca25258c787d87a
Victor Arowo <arowovici@gmail.com>
2019-10-22 10:52:50 +0100

    Fixed Bug in Import of Signup Form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import {} from 'react-router-dom';
 import './App.css';
-import {WrappedNormalLoginForm} from './components/Login';
-import {SignUpForm} from './components/SignUp';
+import { WrappedNormalLoginForm } from './pages/Login';
+import { SignUpForm } from './pages/SignUp';
 
 function App() {
   return (
     <div className="App">
       <div className="header">
-      <h1>DevDesk Queue</h1>
-      <h2>Log in</h2>
+        <h1>DevDesk Queue</h1>
+        <h2>Log in</h2>
       </div>
       <div className="App-form">
-      <WrappedNormalLoginForm />
+        <WrappedNormalLoginForm />
       </div>
     </div>
   );
