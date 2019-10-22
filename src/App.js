6c433ab5ef2c9d42e0c3e8fc913e6efb472487e9
Victor Arowo <arowovici@gmail.com>
2019-10-22 16:49:56 +0100

    Added TIcket List and minor Bug Fixes

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React from 'react';
 import './App.css';
 import { WrappedNormalLoginForm } from './pages/Login';
 import { SignUpForm } from './pages/SignUp';
+import { Route } from 'react-router-dom';
+import StudentTicketList from './components/TicketList/StudentTicketList';
 
 function App() {
   return (
     <div className="App">
-      <div className="header">
+      {/* <div className="header">
         <h1>DevDesk Queue</h1>
         <h2>Log in</h2>
       </div>
       <div className="App-form">
         <WrappedNormalLoginForm />
-      </div>
+      </div> */}
+
+      <Route path="/" component={StudentTicketList} />
     </div>
   );
 }
